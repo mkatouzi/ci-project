7e3c72ea547212c5580212623fc844e26116f697
mkatouzi <mkatouzi@10.182.1.246>
2015-04-16 14:33:47 -0400

    Refixing1

--- src/test/java/org/gradle/PersonTest.java
+++ src/test/java/org/gradle/PersonTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 public class PersonTest {
     @Test
     public void canConstructAPersonWithAName() {
-        Person person = new Person("Mazyar");
+        Person person = new Person("Larry");
         assertEquals("Larry", person.getName());
     }
 }
